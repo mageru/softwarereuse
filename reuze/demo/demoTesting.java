3c6e9dcb1d15461c561502788dbb5d21659bdfff
Justin Miller <jmiller9@gmail.com>
2012-08-22 02:34:09 -0400

    I have player rotation and tile editing in place. Would like to start drawing X's with lines and circles with elipses for player rotation instead of changing the color of the tile.

--- reuze/demo/demoTesting.java
+++ reuze/demo/demoTesting.java
@@ -5,11 +5,16 @@ import processing.core.PGraphics;
 
 public class demoTesting extends PApplet {
 
-	PGraphics orb;
+	//PGraphics orb;
 
-	float[] x = new float[2];
-	float[] y = new float[2];
-	float segLength = 50;
+	//float[] x = new float[2];
+	//float[] y = new float[2];
+	float yPos = (float) 0.0;
+	int valueRed = 0;
+	int valueGreen = 0;
+	int valueBlue = 0;
+	String selected = "none";
+	int player=1;
 
 	public void setup() {
 	  size(200, 200);
@@ -17,15 +22,65 @@ public class demoTesting extends PApplet {
 	  background(0);
 	  noStroke();
 	  fill(226);
+	  //TOP LEFT
+	  fill(0);
 	  rect(0,0,height/3,width/3);
+	  //TOP MIDDLE
+	  fill(255);
+	  rect(width/3,0,height/3,width/3);
+	  rect(0,width/3,height/3,width/3);
+	  rect((width/3)*2,width/3,height/3,width/3);
+	  rect(width/3,width/3*2,height/3,width/3);
+	  //TOP RIGHT
+	  fill(0);
 	  rect((width/3)*2,0,height/3,width/3);
+	  //BOTTOM RIGHT
+	  fill(0);
 	  rect(0,(width/3)*2,height/3,width/3);
+	  //BOTTOM LEFT
+	  fill(0);
 	  rect((width/3)*2,(width/3)*2,height/3,width/3);
-	  rectMode(CENTER);
-	  rect(width/2,height/2,width/3,height/3);
+	  //rectMode(CENTER);
+	  fill(0);
+	  rect(width/3,height/3,width/3,height/3);
 	  //rect(width/2,height/2,width/3,height/3);
 	  //rect(width/2,height/2,width/3,height/3);
 	  //rect(width/2,height/2,width/3,height/3);
+	}
+	
+	public void draw() {
+		if(selected.equals("middle")) {
+			fill(valueRed,valueGreen,valueBlue);
+			rect(width/3,height/3,width/3,height/3);
+		}
+		else if(selected.equals("topleft")) {
+			fill(valueRed,valueGreen,valueBlue);
+			rect(0,0,height/3,width/3);
+		}
+	}
+	
+	public void mousePressed() {
+		selected="none";
+			if(player == 1) { 
+				valueRed = 204;
+				valueGreen = 102;
+				valueBlue = 0;
+				player=2;
+			} 
+			else if (player == 2){
+				valueRed = 255;
+				valueGreen = 0;
+				valueBlue = 0;
+				player=1;
+			}
+		if((height/3) < mouseX && mouseX < (width/3)*2
+			&& mouseY > (height/3) && mouseY < (height/3*2)) {
+			selected="middle";			
+		}
+		if((width/3) > mouseX && mouseY < (height/3)
+				) {
+			selected="topleft";			
+		}
 	}
 
 	/**
